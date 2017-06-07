e6be4e2c83531d3f4b2825eaa51084a6c60d4d10
DESKTOP-VOAUB9K\satveer <kanwal.bhohi@yahoo.co.in>
2017-06-07 15:57:24 +0530

    google map with updated  marker.

--- LoginRegistration/app/src/main/java/com/medicare/app/activity/SuccessActivity.java
+++ LoginRegistration/app/src/main/java/com/medicare/app/activity/SuccessActivity.java
@@ -52,7 +52,8 @@ public class SuccessActivity extends BaseActivty {
         ) {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(SuccessActivity.this, NoNetworkActivity.class);
+               // Intent intent = new Intent(SuccessActivity.this, NoNetworkActivity.class);
+                Intent intent = new Intent(SuccessActivity.this, UberMapActivity.class);
                 startActivity(intent);
             }
         });
